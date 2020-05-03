282e2a9d7df1df7dd58759afb103bee3db369fa8
Anne L'Hôte <anne.lhote@gmail.com>
2020-05-03 20:26:18 +0200

    Iterate over artist

--- index.js
+++ index.js
@@ -1,41 +1,65 @@
+const _ = require('lodash')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const es = require('@elastic/elasticsearch')
 
-// var url ='http://api.genius.com/artists/47263';
 const headers = { 'Authorization': 'Bearer wBSsHGD8VuHpJZdO5YAHvLq0oxG9x9oUdnmhNFqvONO7VrfCHzfOodIFpLx795ZX' }
-// Goldman artist id : 47263
-// fetch(url, { method: 'GET', headers, params: { text_format : 'plain,html' } })
-//   .then((res) => {
-//     // console.log(res)
-//     return res.json()
-//   })
-//   .then((json) => {
-//     console.log(json);
-//     // Do something with the returned data.
-//   });
+const client = new es.Client({ node: 'http://localhost:9200' })
+const baseUrl = 'http://api.genius.com'
 
-const { Client } = require('@elastic/elasticsearch')
-const client = new Client({ node: 'http://localhost:9200' })
+async function getSongsFromArtistId (artistId) {
+    const artist_url = `${baseUrl}/artists/${artistId}/songs`
+    let page = 1
+    while (page !== null) {
+        try {
+            const params = { per_page: 50, page }
+            const res = await axios.get(artist_url, { headers, params })
+            console.log('scapping page : ' + page)
+            _.map(res.data.response.songs, async song => await getLyricsFromSongId(song.id))
+            page = res.data.response.next_page
+        } catch (error) {
+            console.log('ERROR')
+            console.log(error)
+        }
+    }
+}
 
 // Retrieve song lyrics
-// Song id : Là bas : 240015
-const url ='http://api.genius.com/songs/240015';
-axios.get(url, { headers, params: { text_format : 'plain,html' } })
-  .then(async json => {
-    const albumPath = json.data.response.song.path
+async function getLyricsFromSongId (songId) {
+    console.log('getLyricsFromSongId : ' + songId)
+    const url = `${baseUrl}/songs/${songId}`;
+    try {
+        const res = await axios.get(url, { headers, params: { text_format : 'plain,html' } })
+        const songPath = res.data.response.song.path
+        console.log('songPath : ' + songPath)
 
-    // Retrieve lyrics
-    const url2 = `http://genius.com${albumPath}`
-    const result = await axios.get(url2)
-    const html = cheerio.load(result.data)
-    const lyrics = html('div.lyrics').text()
-
-    await client.index({
-        index: 'lyrics-datashare',
-        type: 'doc',
-        body: {
-            content: lyrics
+        // Retrieve lyrics
+        const songUrl = `http://genius.com${songPath}`
+        try {
+            const result = await axios.get(songUrl)
+            const html = cheerio.load(result.data)
+            const lyrics = html('div.lyrics').text()
+            console.log('I got lyrics for song : ' + songId)
+            await client.index({
+                index: 'lyrics-datashare',
+                type: 'doc',
+                body: {
+                    content: lyrics
+                }
+            })
+        } catch (error) {
+            console.log('ERROR 3')
+            console.log(error)
         }
-    }).catch(() => {})
-  }).catch(() => {})
+    } catch (error) {
+        console.log('ERROR 2')
+        console.log(error)
+    }
+}
+
+// Song id : Là bas : 240015
+// getLyricsFromSongId(240015)
+// Artist id : Goldman : 47263
+// Artist id : Benjamin Clementine : 263339
+getSongsFromArtistId(47263)
 
